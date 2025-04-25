ab8bee502eb46a8353016c37855ce6cc58b946bd
vineexoxo <vineetpriyedarshi@gmail.com>
2025-04-25 17:22:33 +0530

    first commit

---
+++ src/hex/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"hex/internal/adapters/core/arithmetic"
+)
+
+func main() {
+	arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 3)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	fmt.Println(result)
+
+}
