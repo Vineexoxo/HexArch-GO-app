c8c4e3a8c8aa8a645640820e9eeab5a2aea243a1
vineexoxo <vineetpriyedarshi@gmail.com>
2025-05-03 19:04:59 +0530

    corrected grpc server and added main

--- src/hex/cmd/main.go
+++ src/hex/cmd/main.go
@@ -1,10 +1,40 @@
 package main
 
+import (
+	"hex/internal/adapters/app/api"
+	"hex/internal/adapters/core/arithmetic"
+	"hex/internal/adapters/framework/left/grpc"
+	db "hex/internal/adapters/framework/right/db"
+	"hex/internal/ports"
+	"log"
+	"os"
+)
+
 func main() {
 
+	var err error
+
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIport
+	var gRPCAdapter ports.GRPCPort
 	//ports
 	// var core ports.ArithmeticPort
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
 
+	if err != nil {
+		log.Fatalf("failed to initiate dbase connection: %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
 	// core = arithmetic.NewAdapter()
 
+	core = arithmetic.NewAdapter()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = grpc.NewAdapter(appAdapter)
+
+	gRPCAdapter.Run()
 }
--- src/hex/internal/adapters/framework/left/grpc/server.go
+++ src/hex/internal/adapters/framework/left/grpc/server.go
@@ -1,37 +1,38 @@
-package RawDescriptor
+package grpc
+
 import (
-	"log"
-	"net"
 	"hex/internal/adapters/framework/left/grpc/pb"
 	"hex/internal/ports"
+	"log"
+	"net"
 
 	"google.golang.org/grpc"
 )
 
 type Adapter struct {
-	api ports.APIPort
+	api ports.APIport
 }
 
-func NewAdapter(api ports.APIPort) *Adapter {
+func NewAdapter(api ports.APIport) *Adapter {
 	return &Adapter{api: api}
 }
 
 func (grpca Adapter) Run() {
 	var err error
 
-	listen, err := net.Listen("tcp",":9000")
+	listen, err := net.Listen("tcp", ":9000")
 
-	if err!=nil {
-		log.Fatalf("failed to listen on port 9000: %v" err)
+	if err != nil {
+		log.Fatalf("failed to listen on port 9000: %v", err)
 	}
 
 	arithmeticServiceServer := grpca
 
-	grpcServer :=grpc.NewServer()
+	grpcServer := grpc.NewServer()
 
-	pb.RegisterArithmeticServiceServer(grpcServer,arithmeticServiceServer)
+	pb.RegisterArithmeticServiceServer(grpcServer, arithmeticServiceServer)
 
-	if err :=grpcServer.Serve(listen); err!=nil {
-		log.Fatalf("failed to serve gRPC server over port 9000: %v" err)
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve gRPC server over port 9000: %v", err)
 	}
 }
